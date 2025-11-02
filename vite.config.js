200e5bfda91abf46c6d613775edfc515a9f3173f
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2025-11-02 18:23:10 +1000

    новый деплой 1.5.8

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // для собственного домена
+  base: '/ev/', // для собственного домена
   plugins: [react()],
 })
 // $env:Path += ";F:\Program\nodejs22"
