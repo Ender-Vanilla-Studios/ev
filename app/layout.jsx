c251e289aa8340ee3f7b1d911cfd887a0760d79d
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2026-01-18 14:39:47 +1000

    -

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,41 @@ import '../css/styles.css';
 import '../css/mlg-v1.css';
 
 export default function RootLayout({ children }) {
+
+    export const metadata = {
+        title: 'Ender Vanilla',
+        description: 'Ender Vanilla - Без вайповая ламповая ванилла с элементами роле плэя | Minecraft сервер',
+        icons: {
+            icon: '/img/logo/ev.webp',
+        },
+        keywords: `
+    minecraft, minecraft server, ender vanilla, vanilla server, без вайпов, ролевой сервер, minecraft roleplay,
+    mc rpg, minecraft survival, minecraft pvp, minecraft community, minecraft multiplayer, minecraft economy,
+    minecraft factions, minecraft creative, minecraft hardcore, minecraft adventure, minecraft minigames,
+    minecraft clans, minecraft quests, minecraft exploration, minecraft crafting, minecraft mining, minecraft whitelist,
+    minecraft pve, minecraft ip, minecraft game, minecraft launcher, minecraft vanilla rp, minecraft vanilla gameplay,
+    minecraft vanilla experience, minecraft vanilla mods, minecraft vanilla plugins, minecraft vanilla economy,
+    minecraft vanilla community, minecraft vanilla players, minecraft vanilla updates, minecraft russian servers,
+    minecraft chill server, minecraft social server, minecraft friendly server, minecraft custom server, minecraft build server,
+    minecraft no wipe, minecraft no wipes server, minecraft best servers, minecraft top servers
+  `,
+        themeColor: '#9311bc',
+        twitter: {
+            card: 'summary_large_image',
+            title: 'Ender Vanilla',
+            description: 'Без вайповая ванилла с элементами роле плэя',
+            images: ['https://ender-vanilla.su/pre.png'],
+        },
+        openGraph: {
+            title: 'Ender Vanilla',
+            description: 'Без вайповая ванилла с элементами роле плэя',
+            url: 'https://ender-vanilla.su/',
+            images: ['https://ender-vanilla.su/pre.png'],
+            type: 'website',
+        },
+    };
+
+
     return (
         <html lang="ru">
         <head>
