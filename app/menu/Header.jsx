df9950ad6f38b32d4be5ccc7d086f145575d7719
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2026-01-19 20:33:13 +1000

    -

--- app/menu/Header.jsx
+++ app/menu/Header.jsx
@@ -30,8 +30,8 @@ function Header() {
                         <Link href="d">Донат</Link>
                         <a href="r?url=https://drive.google.com/drive/folders/1nTKLR3do6z_DYg_5rWkd58MGcfDddAiK?usp=sharing" target="_blank">Карта ОФФ</a>
                         <a href="#socseti">Контакты</a>
-                        <a href="r&url=https://discord.com/channels/1072827575426109490/1454393232418148362/1454393232418148362" target="_blank">FAQ</a>
-                        <a href="r&url=https://drive.google.com/drive/folders/1HPH6rZcToXEjsEOa3P-3zZby4rgdSg1Z?usp=sharing" target="_blank">Обои</a>
+                        <a href="r?url=https://discord.com/channels/1072827575426109490/1454393232418148362/1454393232418148362" target="_blank">FAQ</a>
+                        <a href="r?url=https://drive.google.com/drive/folders/1HPH6rZcToXEjsEOa3P-3zZby4rgdSg1Z?usp=sharing" target="_blank">Обои</a>
                         <a href="minecraft://?addExternalServer=Ender Vanilla|be.ender-vanilla.su:25989" target="_blank">+Сервер</a>
                     </nav>
                 </ul></div></div>
